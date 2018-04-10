f9d1f93fa7345c62c4c54c4daf6f37a2fa597b9e
Evan Garrett <evang522@gmail.com>
2018-04-10 13:44:11 -0400

    Implement Binary Tree height check

---
+++ BinaryTree.js
@@ -0,0 +1,159 @@
+'use strict';
+class BinarySearchTree {
+  constructor(key=null, value=null, parent=null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    }
+
+    else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.left.insert(key, value);
+      }
+    }
+    else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    if (this.key === key) {
+      return this.value;
+    }
+    else if (key < this.key && this.left) {
+      return this.left.find(key);
+    }
+    else if (key > this.key && this.right) {
+      return this.right.find(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  remove(key) {
+    if (this.key === key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this === this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this === this.parent.right) {
+        this.parent.right = node;
+      }
+
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
+
+}
+
+//============================================ Height of BST ======================================>
+
+// take a BST as a parameter
+// establish parent array
+// find the furthest that the tree extends on the left and right
+// establish a counter variable
+
+const findHeight = bst => {
+  if (!bst.left && !bst.right) {
+    return 1;
+  }
+  if (bst.left === null) {
+    return findHeight(bst.right);
+  }
+  if (bst.right === null) {
+    return findHeight(bst.left);
+  }
+
+  console.log('MATH MAX: ', Math.max(findHeight(bst.left), findHeight(bst.right)));
+
+  return Math.max(findHeight(bst.left), findHeight(bst.right)) + 1;
+};
+
+
+//============================================ Main Function ======================================>
+
+function main() {
+  const myTree = new BinarySearchTree();
+  myTree.insert(3);
+  myTree.insert(10);
+  myTree.insert(15);
+  myTree.insert(12);
+  myTree.insert(1);
+  myTree.insert(50);
+  myTree.insert(45);
+
+
+  // console.log(myTree);
+  console.log(findHeight(myTree));
+}
+
+
+main();
